21845b585a0157013456ca5768c4b3f946a87f3c
Swibm <swibm9009@gmalil.com>
2023-01-17 22:24:16 +0600

    files have been created

---
+++ task1/app.js
@@ -0,0 +1,26 @@
+﻿const box = document.querySelector(".box")
+let positionX = 0
+let positionY = 0
+
+const handleBoxMove = () => {
+    if(positionX < 450 && positionY == 0) {
+            positionX+=10
+            box.style.left = `${positionX}px`
+            setTimeout(handleBoxMove, 25)
+        }
+    else if (positionY < 450 && positionX == 450) {
+        positionY+=10
+        box.style.top = `${positionY}px`
+        setTimeout(handleBoxMove, 25)
+    }
+    else if (positionY == 450 && positionX > 0) {
+        positionX-=10
+        box.style.left = `${positionX}px`
+        setTimeout(handleBoxMove, 25)
+    }
+    else  {
+        positionY-=10
+        box.style.top = `${positionY}px`
+        setTimeout(handleBoxMove, 25)
+    }
+    }
---
+++ task2/app.js
@@ -0,0 +1,15 @@
+﻿//dz2 вывести на экран таймер, который отчитывается с 0 до 60. При достижении 60 секунд вывести на экран какое-либо сообщение
+const digit = document.querySelector(".seconds")
+let startPoint = 0
+digit.innerHTML = 0
+
+const addSecond = () => {
+    if (+digit.innerHTML < 60) {
+        startPoint++
+        digit.innerHTML = startPoint
+        setTimeout(addSecond, 1000)
+    }
+    else {digit.innerHTML = "60 seconds have passed"}
+}
+
+addSecond()
